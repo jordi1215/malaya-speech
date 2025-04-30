03b23f6f8f1b184ea51455396ebdab99c3ec894c
Jordi Ramos <jordi1215.jrc@gmail.com>
2025-05-01 02:05:01 +0800

    Update featurization.py

--- malaya_speech/utils/featurization.py
+++ malaya_speech/utils/featurization.py
@@ -36,8 +36,8 @@ class SpeakerNetFeaturizer:
         self.normalize_feature = normalize_feature
 
         self.mel_basis = librosa.filters.mel(
-            self.sample_rate,
-            self.n_fft,
+            sr=self.sample_rate,
+            n_fft=self.n_fft,
             n_mels=self.num_feature_bins,
             fmin=0,
             fmax=self.sample_rate / 2,
